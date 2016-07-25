3458dca38946cecf3c7a926a896e1bb56dd43623
Vaceslav Ustinov <vaceslav.ustinov@gmail.com>
2016-07-25 20:19:18 +0200

    add lesson 04

---
+++ 04-preprocessors/README.md
@@ -0,0 +1,16 @@
+# Pre-Processors
+## SASS CSS Preprocessor ( http://sass-lang.com/ )
+Steps:
+- npm install --global gulp-cli
+- npm init
+
+- npm install --save-dev node-sass
+
+- npm install --save-dev gulp
+- create gulp file
+- npm install gulp-sass --save-dev
+
+```
+gulp sass
+gulp sass:watch
+```
---
+++ 04-preprocessors/gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+//-------------------------DEFAULT TASK------------------------
+gulp.task('default', function() {
+  // place code for your default task here
+});
+ 
+
+//---------------------------SASS TASK---------------------------------
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+//------------------------------TYPE SCRIPT TASKS
